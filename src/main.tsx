6cf6bbcb032b4f5c5fffe9a29b087a062ef73bc9
Afonin Andrey <andreyafonin96@gmail.com>
2024-05-19 12:13:30 +0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './utils/firebase';
 import './config/configureMobX';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.scss';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
